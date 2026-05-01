45026c30fe053ca9a8f4eb9e8240c3a2ae89bc3c
teranixbq <hanieffathulb03@gmail.com>
2026-05-01 19:06:07 +0700

    update : main with matplotlib

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ from evaluation import compute_ndcg
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter, MultipleLocator
 
-
-
 def main():
-    index = build_index(CORPUS)
-    print(f'Koleksi: {index["N"]} dokumen | avgdl = {index["avgdl"]:.2f}token\n')
     all_results = {}
-
-    for qid, query_text in QUERIES.items():
-        print(f'=== {qid}: "{query_text}" ===')
-        results = rank(query_text, index)
+    index = build_index(CORPUS) 
+    print(f'Koleksi: {index["N"]} dokumen | avgdl = {index["avgdl"]:.2f} token\n') 
+ 
+    for qid, query_text in QUERIES.items(): 
+        print(f'=== {qid}: "{query_text}" ===') 
+        results = rank(query_text, index) 
+        qrels   = QRELS[qid] 
         all_results[qid] = results
-        qrels = QRELS[qid]
-        print(f' {'Rank':<6} {'Doc':<5} {'BM25 Score':<14} {'Relevansi'}')
-        print(f' {"-"*40}')
-        for i, (doc_id, score) in enumerate(results, 1):
-            rel = qrels.get(doc_id, 0)
-            print(f' {i:<6} {doc_id:<5} {score:<14.4f} {rel}')
-        for k in [3, 5, 10]:
-            ndcg = compute_ndcg(results, qrels, k)
-            print(f' NDCG@{k} = {ndcg:.4f}')
-        print()
+ 
+        print(f'  {'Rank':<6} {'Doc':<5} {'BM25 Score':<14} {'Relevansi'}') 
+        print(f'  {"-"*40}') 
+        for i, (doc_id, score) in enumerate(results, 1): 
+            rel = qrels.get(doc_id, 0) 
+            print(f'  {i:<6} {doc_id:<5} {score:<14.4f} {rel}') 
+ 
+        for k in [3, 5, 10]: 
+            ndcg = compute_ndcg(results, qrels, k) 
+            print(f'  NDCG@{k} = {ndcg:.4f}') 
+        print() 
 
     matplotlib_bar_chart(all_results)
 
